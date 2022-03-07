6af3c2e73acbe2eb3ec5a4965139ca80d6ff046b
Sergey Kovalev <dip.deep@mail.ru>
2022-03-07 18:48:06 +0300

    modified Personage class

--- src/main/java/com/dontexist/characters/Personage.java
+++ src/main/java/com/dontexist/characters/Personage.java
@@ -1,4 +1,12 @@
 package com.dontexist.characters;
 
 public abstract class Personage {
+    private String name;
+    private int dexterity;
+    private int health;
+    private int experience;
+    private int gold;
+    private int strength;
+
+    public void attack() {}
 }
